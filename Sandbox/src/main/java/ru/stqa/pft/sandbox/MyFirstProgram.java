cf504d87b235bed4697ef4627373ab9fca7d8281
Victoria77705 <69756395+Victoria77705@users.noreply.github.com>
2020-10-28 16:55:42 +0200

    Few functions were added

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        hello("World");
+        hello("User");
+        hello("Victoria");
 
-        System.out.println("Hello, world!");
+        double l = 5;
+        System.out.println("Area of square with a side " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Square of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+    }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
     }
 }
